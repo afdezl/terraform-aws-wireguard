2203c74e20b45557b4cec492d33921535af23964
Adrian Fernandez <adrian.fdezl@gmail.com>
2019-07-16 12:13:22 +0200

    Fixes/readme (#1)
    * Cleaning outputs and README
    
    * Adding reference project

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,6 @@ output "vpn_sg_id" {
   description = "ID of the internal Security Group to associate with other resources needing to be accessed on VPN."
 }
 
-output "autoscaling_group_name" {
+output "vpn_asg_name" {
   value = aws_autoscaling_group.wireguard_asg.name
 }
